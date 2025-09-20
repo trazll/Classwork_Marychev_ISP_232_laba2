bc4750829d354b1e5f9b43bf911ee8652e1e3d9f
traz <degleted@gmail.com>
2025-09-20 17:53:37 +0300

    Create program #3

--- src/ConditionalExpressions.kt
+++ src/ConditionalExpressions.kt
@@ -1,12 +1,5 @@
 fun main() {
-    print("Введите первое число: ")
-    val number1 = readln().toInt()
-    print("Введите второе число: ")
-    val number2 = readln().toInt()
-
-    if (number1 > number2) {
-        println("Число $number1 больше чем число $number2")
-    } else {
-        println("Число $number2 больше чем число $number1")
-    }
+    val age = readln().toInt()
+    val status = if (age >= 18) "Взрослый" else "Ребёнок"
+    println(status)
 }
